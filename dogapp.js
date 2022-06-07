7d3b2a45612909866f90bb515ef3654aa9bfba06
Jay-ar Dela Rama <jayardelarama27@gmail.com>
2022-06-07 21:51:44 +0800

    added fetching data from API

---
+++ dogapp.js
@@ -0,0 +1,42 @@
+const BREED_URL = "https://dog.ceo/api/breeds/list/all";
+const select = document.querySelector('.breeds-selection');
+// displaying the breed list to the select tag
+// * oh no this is a highlight comment
+// !this is the red comment
+//? this is the question mark comment
+// todo  -- fuck your boss
+fetch(BREED_URL)
+    .then (function (response){
+        return response.json();
+    })
+    .then (function (data){
+        const breedObject = data.message;
+        const breedArray = Object.keys(breedObject);
+
+        for(let i = 0; i < breedArray.length; i++){
+            const option = document.createElement('option');
+            option.value =  breedArray[i];
+            option.innerText = breedArray[i];
+            select.appendChild(option);
+        }
+    })
+
+    select.addEventListener("change", function(event){
+        // url to random image
+        let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+        
+        // get the url
+        getDog(url);
+    });
+//fetch the url and change the src of the image
+const img = document.querySelector(".dog-img");
+
+    function getDog(url){
+        fetch(url)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                img.src = data.message;
+            })
+    }
